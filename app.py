23d471609131a7a9516e48cecc9be1133664aee3
felipemonsef10 <felipemonsef10@gmail.com>
2025-06-03 14:08:06 -0300

    alterando a api

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/webhook', methods=['POST'])
 def receber_mensagem():
     dados = request.json
-    print("Mensagem recebida:", dados)
+    # print("Mensagem recebida:", dados)
     return {"status": "ok"}, 200
 
 if __name__ == '__main__':
